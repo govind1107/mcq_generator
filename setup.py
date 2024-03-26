eef695fbcd24511364fabb42fe7dde959709f484
Govind <govind.lipne11@gmail.com>
2024-03-26 21:56:50 +0530

    files updated

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 # Created to install local packages in virtual environments
+
+
+from setuptools import find_packages,setup
+
+setup(
+    name = "MCQ_GENERATOR",
+    version = "0.0.1",
+    author="Govind Lipne",
+    author_email="govind.lipne11@gmail.com",
+    install_requires = ['openai','langchain','streamlit','python-dotenv','PyPDF2','langchain-openai'],
+    packages=find_packages()
+)
